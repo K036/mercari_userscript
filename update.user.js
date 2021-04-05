646e3beb36550e985db589bf8e8190b4988246d4
K036 <81788577+K036@users.noreply.github.com>
2021-04-05 18:10:14 +0900

    Create update.user.js
    アップデート用プログラム

---
+++ update.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         プログラム更新
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       K36
+// @match        https://www.mercari.com/jp/*
+// @run-at       context-menu
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    const form = "https://www.notion.so/036k/20675228328c4b7eb3ac69819977f77b";
+    location.assign(form);
+})();
